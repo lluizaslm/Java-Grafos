ff4dc05e88a4e04cb5236b508b2fe0c5488e5bcf
Marcos <m.vinicius.sr11@gmail.com>
2024-08-23 14:07:15 -0300

    criando questão 05

---
+++ Atv06_grafo/Q05_grafo/main.java
@@ -0,0 +1,27 @@
+package Q05_grafo;
+
+public class main {
+    public static void main(String[] args) {
+        Grafo<String> grafo = new Grafo<>();//usando string só pra testar
+        // adc vertices
+        grafo.adicionarVertice("A");
+        grafo.adicionarVertice("B");
+        grafo.adicionarVertice("C");
+        grafo.adicionarVertice("D");
+
+        // adc as arestas
+        grafo.adicionarAresta(1.0, "A", "B");
+        grafo.adicionarAresta(2.5, "A", "C");
+        grafo.adicionarAresta(1.5, "B", "D");
+        grafo.adicionarAresta(3.0, "C", "D");
+
+        System.out.println("Grafo:");
+        grafo.imprimir();
+
+        grafo.removerAresta("C", "D");
+
+        System.out.println("\nGrafo após remoções:");
+        grafo.imprimir();
+
+    }
+}
